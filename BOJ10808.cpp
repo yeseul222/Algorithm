7d97c226d0dfaf767167006118c22f2a40fbc473
yeseul22 <yeseul950821@gmail.com>
2019-03-07 21:28:44 +0900

    백준 10808 문제 수정
    스트링 값의 길이를 변수에 넣어줌.

--- BOJ10808.cpp
+++ BOJ10808.cpp
@@ -8,7 +8,8 @@ int main() {
 	int arr[30] = { 0 };
 	string s;
 	cin >> s;
-	for (int i = 0; i < s.length(); i++) {
+    int s_length = s.length();
+	for (int i = 0; i < s_length; i++) {
 		arr[s[i]-97]++;
 	}
 	for (int i = 0; i < 26; i++) {
