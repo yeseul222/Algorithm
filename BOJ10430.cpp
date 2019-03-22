816b47c2ba9bfd70eaca784858540f1cea28d606
yeseul22 <yeseul950821@gmail.com>
2019-03-22 12:55:00 +0900

    백준 10430 문제 업로드
    백준 10430 문제 코드

---
+++ BOJ10430.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+#pragma warning (disable:4996)
+
+using namespace std;
+
+int main() {
+	int a, b, c;
+
+	cin >> a >> b >> c;
+	cout << (a + b) % c << "\n";
+	cout << (a % c + b % c) % c << "\n";
+	cout << (a * b) % c << "\n";
+	cout << (a % c * b % c) % c << "\n";
+
+	return 0;
+}
